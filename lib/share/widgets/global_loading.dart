7498936a736892b8628832cee7e62eaf628621ab
Duc Pham <phamvanduc.dev@gmail.com>
2023-12-08 16:29:13 +0700

    Remove flutter_hooks

--- lib/share/widgets/global_loading.dart
+++ lib/share/widgets/global_loading.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:rxdart/rxdart.dart';
 
 class LoadingParameter {
@@ -10,11 +9,10 @@ class LoadingParameter {
   });
 }
 
-class GlobalLoadingWidget extends HookWidget {
-  const GlobalLoadingWidget({super.key});
-
-  static final _stream =
-      BehaviorSubject.seeded(LoadingParameter(isVisible: false));
+class GlobalLoadingWidget extends StatelessWidget {
+  static final _stream = BehaviorSubject.seeded(
+    LoadingParameter(isVisible: false),
+  );
 
   static show() {
     _stream.add(LoadingParameter(isVisible: true));
@@ -24,46 +22,24 @@ class GlobalLoadingWidget extends HookWidget {
     _stream.add(LoadingParameter(isVisible: false));
   }
 
-  static get isVisible {
-    return _stream.value.isVisible;
-  }
+  const GlobalLoadingWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final opacityController = useAnimationController(
-      duration: const Duration(milliseconds: 200),
-    );
-    useAnimation(opacityController);
-
-    var params = useStream(_stream).data;
-
-    if (params == null) {
-      return Container();
-    }
-
-    useEffect(() {
-      if (params.isVisible) {
-        opacityController.forward();
-      } else {
-        opacityController.reverse();
-      }
-      return null;
-    }, [params.isVisible]);
-
-    if (opacityController.value == 0.0) {
-      return Container();
-    }
-
-    return Opacity(
-      opacity: opacityController.value,
-      child: Container(
-        height: double.infinity,
-        width: double.infinity,
-        color: Colors.black.withOpacity(0.4),
-        child: const Center(
-          child: CircularProgressIndicator(color: Colors.white),
-        ),
-      ),
+    return StreamBuilder<LoadingParameter>(
+      stream: _stream.stream,
+      builder: (context, snapshot) {
+        return snapshot.data?.isVisible == true
+            ? Container(
+                height: double.infinity,
+                width: double.infinity,
+                color: Colors.black.withOpacity(0.4),
+                child: const Center(
+                  child: CircularProgressIndicator(color: Colors.white),
+                ),
+              )
+            : Container();
+      },
     );
   }
 }
